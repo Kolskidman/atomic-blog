cf47f46a6fe486a24e77f11bc9ea41d3cb1ed25b
Kolskidman <kolskidman@gmail.com>
2023-10-26 19:21:40 +0100

    initial commit

---
+++ src/PostContext.js
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState } from 'react';
+import { faker } from '@faker-js/faker';
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+// Creating POST CONTEXT
+const PostContext = createContext();
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+export function usePosts() {
+  const post = useContext(PostContext);
+
+  if (post === undefined)
+    throw new Error('Context not available when being used');
+  return post;
+}
+
+export default PostProvider;
